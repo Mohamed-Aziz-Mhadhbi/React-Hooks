e58dfdc6c72817575351353b23b4467e7ad1818c
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-02-09 15:20:51 +0100

    setState input value

--- React-Hooks/src/setState.jsx
+++ React-Hooks/src/setState.jsx
@@ -1,14 +1,18 @@
-
-
 import { useState } from 'react'
 import './App.css'
 
 function App() {
 
   const [counter, setCounter] = useState(0);
+  const [inputValue, SetInputValue] = userState("Hello!");
 
   const increment = () => {
     setCounter(counter +1);
+  }
+
+  const letOnchange = (event) => {
+    const value = event.target.value;
+    SetInputValue(value);
   }
 
   return (
@@ -16,7 +20,11 @@ function App() {
       <p className="read-the-docs">
         {counter}
       </p>
+
       <button onClick={increment}>increment</button>
+
+      <input placeholder='Enter Something ...' onChange={letOnchange}>{inputValue}
+      </input>
     </>
   )
 }
